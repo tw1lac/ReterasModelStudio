d9e868a3fcec77cf5b35b863f83f9ec4466d3d42
tw1lac <pikofluff@gmail.com>
2021-01-17 17:16:40 +0100

    Started to prepare for typing AnimFlag

---
+++ modelstudio/src/com/hiveworkshop/rms/editor/model/animflag/IntAnimFlag.java
@@ -0,0 +1,38 @@
+package com.hiveworkshop.rms.editor.model.animflag;
+
+import com.hiveworkshop.rms.editor.model.TimelineContainer;
+import com.hiveworkshop.rms.parsers.mdlx.timeline.MdlxTimeline;
+
+import java.util.List;
+
+/**
+ * A java class for MDL "motion flags," such as Alpha, Translation, Scaling, or
+ * Rotation. AnimFlags are not "real" things from an MDL and are given this name
+ * by me, as an invented java class to simplify the programming
+ *
+ * Eric Theller 11/5/2011
+ */
+public class IntAnimFlag extends AnimFlag2<Integer> {
+
+
+	public IntAnimFlag(MdlxTimeline<Integer> timeline) {
+		super(timeline);
+	}
+
+	public IntAnimFlag(String title, List<Integer> times, List<Integer> values) {
+		super(title, times, values);
+	}
+
+	public IntAnimFlag(String title) {
+		super(title);
+	}
+
+	public IntAnimFlag(AnimFlag2<Integer> af) {
+		super(af);
+	}
+
+	@Override
+	public MdlxTimeline<Integer> toMdlx(TimelineContainer container) {
+		return null;
+	}
+}
